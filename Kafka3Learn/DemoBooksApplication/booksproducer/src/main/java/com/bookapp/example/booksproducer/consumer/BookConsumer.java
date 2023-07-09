1b511e4ff9a6264adc214f31283c5916e9e78ed5
esakdev <esakki2021@gmail.com>
2023-07-09 13:46:34 +0530

    udpated

--- Kafka3Learn/DemoBooksApplication/booksproducer/src/main/java/com/bookapp/example/booksproducer/consumer/BookConsumer.java
+++ Kafka3Learn/DemoBooksApplication/booksproducer/src/main/java/com/bookapp/example/booksproducer/consumer/BookConsumer.java
@@ -1,24 +1,24 @@
-package com.bookapp.example.booksproducer.consumer;
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-public class BookConsumer {
-
-
-    @KafkaListener(groupId = "bathirdparty", topics = {"books-events-1"})
-    public void onMessage(ConsumerRecord<Integer, String> consumerrecord) throws JsonProcessingException {
-
-        log.info("INFO :: Consumer Record :: {}", consumerrecord);
-
-    }
-}
-
-
-
-
-
+//package com.bookapp.example.booksproducer.consumer;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.kafka.clients.consumer.ConsumerRecord;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//public class BookConsumer {
+//
+//
+//    @KafkaListener(groupId = "bathirdparty", topics = {"books-events-1"})
+//    public void onMessage(ConsumerRecord<Integer, String> consumerrecord) throws JsonProcessingException {
+//
+//        log.info("INFO :: Consumer Record :: {}", consumerrecord);
+//
+//    }
+//}
+//
+//
+//
+//
+//
